321aa605a4107cadd1e4e9489325f98cd9cfd061
Kodion <kodion@Kodions-MacBook-Pro.local>
2025-09-24 11:08:01 +0530

    add includeded and bonusm in membership page

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'CSS/color.dart';
 import 'Screens/Splash/splash_page.dart';
 
